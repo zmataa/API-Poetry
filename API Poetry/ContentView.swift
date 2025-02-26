2322d991b1da7d7cca5fc7e57c2b128999f5e6b1
Zane <zmatarieh@bsd220.org>
2025-02-26 09:56:08 -0600

    setup contentview

--- API Poetry/ContentView.swift
+++ API Poetry/ContentView.swift
@@ -8,17 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var authors = [String]()
+    @State private var showingAlert = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List(authors, id: \.self) { author in
+                NavigationLink(destination: Text(author)) {
+                    Text(author)
+                }
+            }
+            
+            .navigationTitle("Poetry Authors")
+            .navigationBarTitleDisplayMode(.inline)
         }
-        .padding()
+        .task {
+            await getAuthors()
+        }
+        .alert(isPresented: $showingAlert) {
+            Alert(title: Text("Loading Error"), message: Text("There was a problem loading the poetry authors"), dismissButton: .default(Text("OK")))
+        }
     }
-}
+    func getAuthors() async {
 
+          let query = "https://poetrydb.org//author"
+
+          if let url = URL(string: query) {
+
+              if let (data, _) = try? await URLSession.shared.data(from: url) {
+
+                  if let decodedResponse = try? JSONDecoder().decode(Authors.self, from: data) {
+
+                      authors = decodedResponse.authors
+                    return
+                  }
+
+              }
+
+          }
+        showingAlert = true
+      }
+}
+struct Authors: Codable {
+    var authors: [String]
+}
 #Preview {
     ContentView()
 }
